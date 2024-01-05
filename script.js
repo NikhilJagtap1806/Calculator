311235adad5a30f3a2b5e0d77202d30ab7513fdf
NikhilJagtap1806 <jagtapnikhil1806@gmail.com>
2024-01-06 01:08:49 +0530

    Project Completed

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
-let input=document.getElementById("InputField");
-let buttons=document.querySelectorAll("button");
+let input = document.getElementById("InputField");
+let buttons = document.querySelectorAll("button");
 
-let result="";
-let arr=Array.from(buttons)
+let result = "";
+let arr = Array.from(buttons)
 arr.forEach(button => {
-    button.addEventListener(('click'),(e)=>{
-        if (e.target.innerHTML=="="){
-            result=eval(result);
-            input.value=result;
+    button.addEventListener(('click'), (e) => {
+
+
+        if (e.target.innerHTML == "=") {
+            checkInput(result);
+            result = eval(result);
+            input.value = result;
+
         }
-        else if (e.target.innerHTML=="AC"){
-            result="";
-            input.value=result;
+        else if (e.target.innerHTML == "AC") {
+            result = "";
+            input.value = result;
         }
-        else if (e.target.innerHTML=="DEL"){
-            result.result.substring(0,result.length-1);
-            input.value=result;
+        else if (e.target.innerHTML == "DEL") {
+            result = result.substring(0, result.length - 1);
+            input.value = result;
         }
-        else{
-            result +=e.target.innerHTML;
-            input.value=result;
+        else {
+            result += e.target.innerHTML;
+            input.value = result;
         }
 
 
     })
 })
+
+function checkInput(inputString) {
+    // convert the input to a string
+
+
+
+    for (let i = 0; i < inputString.length; i++) {
+
+        if (inputString[i] === '+' || inputString[i] === '*' || inputString[i] === '/') {
+            if (i === 0 || inputString[i - 1] === '+' || inputString[i - 1] === '-' || inputString[i - 1] === '*' || inputString[i - 1] === '/') {
+                alert("Invalid input..!");
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
